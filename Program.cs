147772d60a491d920af830b9647372b998bae356
Jonatas <jonatas.07022003@gmail.com>
2024-03-27 11:39:37 -0300

    Criando página de produto

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient
 {
